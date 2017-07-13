1377531e86e78e2dcc5c9040c9f5433d71878081
Elyse <elyse@redcurrent.com>
2017-07-13 09:35:19 -0400

    added .click() jQuery mmethod for images

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
 console.log('hi');
 
+//select a seat, on click change to the green colored seat
+  $('img').click(function(){
+      $(this).attr('src','selectseat.png');
+  });
+
+//slide down/up panel from codeAcademy
+  $('.pull-me').click(function(){
+   $('.panel').slideToggle('slow');
+      });
 
 
 
