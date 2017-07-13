a16727318c7704fe43a3c4a2ccd48b4f1f024dbc
yteshome <yteshome@gmail.com>
2017-07-13 14:06:33 -0400

    added to jQery file

--- script.js
+++ script.js
@@ -3,7 +3,43 @@ $('.seat').on('click', function(){
   $('.selected').toggleClass('is-active');
 });
 
+$(function() {
 
+
+	var seatNumber;
+	var selectedSeat;
+
+	//$("form").hide();
+	$(".available").on("click", function() {
+		console.log('hello');
+		$("form").toggleClass('is-active');
+		//$("form").show();
+		// selectedSeat = $(this);
+		// seatNumber = $(this).attr("id");
+		// $(".yourSeat").text("seat #" + seatNumber + ":");
+
+	});
+
+	var reservation = {
+
+	};
+
+	$("#button").on("click", function() {
+		reservation.name = $("#name").val();
+		reservation.phone = $("#phone_number").val();
+		reservation.email = $("#email").val();
+		reservation.yourSeat = $(".yourSeat").text();
+
+		$("#formArea").html("<p>Thank you for your reservation!</p>");
+
+		$(selectedSeat).css("background-color" , "#909090")
+
+			.html("<p id = 'reservedSeat'></p>");
+
+		$("#reservedSeat").text("seat #" + seatNumber + " " + reservation.name);
+	});
+
+});
 
 
 
