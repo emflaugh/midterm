bd059be60339a0b401e93c375bea3aacbb50b4f5
Elyse <elyse@redcurrent.com>
2017-07-14 16:04:29 -0400

    added hover functionality

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
 
 $(document).ready(function(){
 
-//form slide down from the top
-// $('body').click(function(){
-//   $('.reservationForm').slide('slowest');
-// });
-
-
 $('.wrapper').on('click', function(){
   $(this).find('.selected').toggleClass('is-active');
 
-
-  //$('.selected').toggleClass('is-active');
 });
 
 	var seatNumber;
 	var selectedSeat;
 
-	//$("form").hide();
+
 	$(".available").on("click", function() {
 		$("form").addClass('is-active');
-		//$("form").show();
-		// selectedSeat = $(this);
-		// seatNumber = $(this).attr("id");
-		// $(".yourSeat").text("seat #" + seatNumber + ":");
+
 
 	});
-var users = [];
+
 	var reservation = {
 
 	};
@@ -39,53 +28,28 @@ $('.wrapper').on('click', function(){
        reservation.email = $("#email").val();
        reservation.yourSeat = $('.beenClicked').attr('id');
        $('.beenClicked').html('<div><img src="images/takenseat.png" class="unavailable"></div');
+       $('.beenClicked').data(reservation);
+        // console.log($('.beenClicked').data());
        $('.wrapper').removeClass('beenClicked');
-       users.push(reservation);
-      //  $('.beenClicked').html('<div><img src="images/takenseat.png" class="unavailable"></div');
 
-console.log(reservation.yourSeat);
-console.log(users);
-      //  $('.wrapper').find('is-active');
-      //    $('.selected').removeClass('is-active');
-       //
-      //    $(this).find('.reserved').addClass('is-active');
+
+// console.log(reservation.yourSeat);
+// console.log(reservation);
 
 });
-        //  $(this).find('.reserved').addClass('is-active');
 
+//When hovering on wrapper...we have to do a method that displays data tied to the wrapper
+var seat = null;
+$('.wrapper').mouseenter(function(){
+  var name = $(this).data('name');
+  seat = $(this).data('yourSeat');
+  console.log(name + " "+ seat);
+  $('#' + seat).html('<span class="hoverInfo">' + name + ' has reserved this seat</span>');
+  // $(this).append(name + ' ' + seat);
+}).mouseleave(function() {
+  $('#' + seat).html('<div><img src="images/takenseat.png" class="unavailable"></div');
+});
 
-      //  $('.selected').removeClass('is-active');
-      //  $('.wrapper').find('.reserved').addClass('is-active');
-
-
-        //  $('.reserved').addClass('is-active');
-       // reservation.yourSeat = $(".yourSeat").text();
-console.log(reservation);
-
-		// $("#formArea").html("<p>Thank you for your reservation!</p>");
-    //
-		// $(selectedSeat).css("background-color" , "#909090")
-    //
-		// 	.html("<p id = 'reservedSeat'></p>");
-    //
-		// $("#reservedSeat").text("seat #" + seatNumber + " " + reservation.name);
-	// });
-
-// var name;
-//
-// $('.seat').mouseover(function(){
-//   if ($(this).hasClass('reserved')) {
-//      name = $(this).attr('#name');
-//      $(this).children('p').text('#name');
-//   }
-// });
-//
-// //return to reserved when mouse out
-// $('.seat').mouseout(function() {
-//   if ( $(this).hasClass('reserved') ) {
-//     $(this).children('p').text('Reserved');
-//   }
-// });
 
 
 
